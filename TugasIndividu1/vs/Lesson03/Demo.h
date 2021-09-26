045803abd9af66764e0845a9b05d1d9829a1910a
hfzzhdaffa <hafizhdaffa050901@gmail.com>
2021-09-26 22:11:19 +0700

    first commit

---
+++ TugasIndividu1/vs/Lesson03/Demo.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "RenderEngine.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/vector_angle.hpp>
+#include <SOIL/SOIL.h>
+
+class Demo :
+	public RenderEngine
+{
+public:
+	Demo();
+	~Demo();
+private:
+	GLuint shaderProgram, VBO, VAO, EBO, texture, VBO2, VAO2, EBO2, texture2, VBO3, VAO3, EBO3,
+		VBO4, VAO4, EBO4, texture4, VBO5, VAO5, EBO5, texture5, VBO6, VAO6, EBO6, texture6, VBO7, VAO7, EBO7, texture7;
+	virtual void Init();
+	virtual void DeInit();
+	virtual void Update(double deltaTime);
+	virtual void Render();
+	virtual void ProcessInput(GLFWwindow *window);
+	void DrawPapan();
+	void BuildKaki1();
+	void DrawKaki1();
+	void BuildKaki2();
+	void DrawKaki2();
+	void BuildKaki3();
+	void DrawKaki3();
+	void BuildKaki4();
+	void BuildPapan();
+	void DrawKaki4();
+	void BuildColoredPlane();
+	void DrawColoredPlane();
+};
+
